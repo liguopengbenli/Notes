5b9e228b1077eca44defc5b2ee09674ee10aaa23
guopeng li <guopeng.li@technicolor.com>
2020-11-30 10:21:50 +0100

    Add a menu save button

--- app/src/main/java/com/lig/intermediate/notes/create/CreateActivity.kt
+++ app/src/main/java/com/lig/intermediate/notes/create/CreateActivity.kt
@@ -1,15 +1,22 @@
 package com.lig.intermediate.notes.create
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.lig.intermediate.notes.R
 import com.lig.intermediate.notes.navigation.NavigationActivity
 import kotlinx.android.synthetic.main.activity_create.*
 
 class CreateActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create)
+
+        supportActionBar?.title = ""
 
         intent.getStringExtra(NavigationActivity.FRAGEMENT_TYPE_KEY).run {
             textView.text = if (this == NavigationActivity.FRAGEMENT_VALUE_TASK) {
@@ -20,6 +27,19 @@ class CreateActivity : AppCompatActivity() {
                 "Something went wrong"
             }
         }
-
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)  // inflate custom menu to menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item?.itemId){
+            R.id.saveItem -> Toast.makeText(this, "saveClicked", Toast.LENGTH_SHORT).show()
+        }
+        
+        return super.onOptionsItemSelected(item)
+    }
+
 }
