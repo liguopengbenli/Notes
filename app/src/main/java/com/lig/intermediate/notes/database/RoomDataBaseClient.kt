a28db5f0e6fdbdd787a65006668b1d507e38bfe0
guopeng li <guopeng.li@technicolor.com>
2020-12-01 13:46:02 +0100

    Create Room database client class

---
+++ app/src/main/java/com/lig/intermediate/notes/database/RoomDataBaseClient.kt
@@ -0,0 +1,31 @@
+package com.lig.intermediate.notes.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+const val DATABASE_VERSION = 1
+const val DB_NAME = "local_db"
+
+@Database(entities = [], version = DATABASE_VERSION)
+abstract class RoomDataBaseClient: RoomDatabase() {
+    companion object{
+
+        private var instance: RoomDataBaseClient? = null  // create a singleton
+
+        fun getInstance(context: Context): RoomDataBaseClient{
+            if(instance==null){
+                instance = createDataBase(context)
+            }
+            return instance!!
+        }
+
+        private fun createDataBase(context: Context): RoomDataBaseClient {
+            return Room.databaseBuilder(context, RoomDataBaseClient::class.java, DB_NAME).build()
+        }
+    }
+
+
+
+}
