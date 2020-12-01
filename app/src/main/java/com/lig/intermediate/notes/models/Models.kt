769619256ece256feb88ec390f107a69920a1c6b
guopeng li <guopeng.li@technicolor.com>
2020-12-01 14:18:26 +0100

    Create Room Entity, with various data class

--- app/src/main/java/com/lig/intermediate/notes/models/Models.kt
+++ app/src/main/java/com/lig/intermediate/notes/models/Models.kt
@@ -1,26 +1,64 @@
 package com.lig.intermediate.notes.models
 
+import androidx.room.*
+import androidx.room.ForeignKey.CASCADE
+import java.util.*
+
 // kotlin auto generate setter and gettter
 // data class auto generate toString, copie...
 // predefine value to set defaut value // if using in java we have to add @JvmOverloads constructor
 
-data class Task @JvmOverloads constructor(
-    var title: String,
+
+class Task @JvmOverloads constructor(
+    title: String,
     val todos: MutableList<Todo> = mutableListOf(), // create defaut empty list
+    tag: Tag? = null
+) : TaskEntity(title = title, tag = tag)
+
+
+
+@Entity(tableName = "tasks")
+open class TaskEntity(  // create open class because of todos list
+    @PrimaryKey
+    val uid: Long = UUID.randomUUID().leastSignificantBits,
+    @ColumnInfo
+    var title: String,
+    @Embedded
     val tag: Tag? = null
 )
 
+@Entity(tableName = "todos")
 data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    var uid:Int = 0,
+    @ForeignKey(                  //for join
+        parentColumns = ["uid"],
+        childColumns = ["taskKey"],
+        entity = TaskEntity::class,
+        onDelete = CASCADE   // it will delete to do when the task is deleted
+    )
+    var taskKey:Long? = null,  // = uid of TaskEntity for making join
+    @ColumnInfo
     var description: String,
+    @ColumnInfo
     var isComplete: Boolean = false
 )
 
+@Entity(tableName = "notes")
 data class Note(
+    @PrimaryKey(autoGenerate = true) // auto increase value when assign to db
+    var uid: Int = 0,
+    @ColumnInfo
     var description: String,
+    @Embedded
     var tag: Tag? = null
 )
 
+
+@Entity(tableName = "tags")
 data class Tag(
+    @PrimaryKey
     val name: String,
-    val colorResId: Int
+    @ColumnInfo(name = "colour_resource_id")
+    val colourResId: Int
 )
