944e0b22822ee3bc9f1adc792ba6f46a8ae78384
guopeng li <guopeng.li@technicolor.com>
2020-12-04 15:17:19 +0100

    Alternative way to do Dependency injection with module binding

--- app/src/main/java/com/lig/intermediate/notes/foundations/ToothPickUtils.kt
+++ app/src/main/java/com/lig/intermediate/notes/foundations/ToothPickUtils.kt
@@ -1,5 +1,6 @@
 package com.lig.intermediate.notes.foundations
 
+import com.lig.intermediate.notes.create.StateModel
 import com.lig.intermediate.notes.ui.notes.INoteModel
 import com.lig.intermediate.notes.ui.notes.NoteLocalModel
 import com.lig.intermediate.notes.ui.task.ITaskModel
@@ -16,7 +17,22 @@ object ApplicationScope{ // object in kotlin is a singleton class
 
 object ApplicationModule: Module(){
     init {
-        bind(INoteModel::class.java).toInstance(NoteLocalModel())
+        bind(INoteModel::class.java).toInstance(NoteLocalModel()) // call constructor singleton
         bind(ITaskModel::class.java).toInstance(TaskLocalModel())
+    }
+}
+
+// Alternative way
+object CreateActivityScope{
+    val scope = Toothpick.openScope(this).apply {
+        installModules(CreateActivityModule)
+    }
+}
+
+object CreateActivityModule: Module(){
+    init {
+        bind(INoteModel::class.java).to(NoteLocalModel::class.java)
+        bind(ITaskModel::class.java).to(TaskLocalModel::class.java)  // Here we don't use constructor but new dependency injection method
+        bind(StateModel::class.java).toInstance(StateModel()) // call singleton  
     }
 }
